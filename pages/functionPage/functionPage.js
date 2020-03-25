186ccaf7f69ccb2704d249e24a11465c9374d302
mcong <max.cong@nokia-sbell.com>
2020-03-25 15:38:32 +0800

    dev save

--- pages/functionPage/functionPage.js
+++ pages/functionPage/functionPage.js
@@ -27,7 +27,8 @@ Page({
         px2rpx: 0,
         graph: {},
         processing: false,
-        imageReady: false
+        imageReady: false,
+        progress:0
 
     },
 
@@ -404,7 +405,12 @@ Page({
                                 var order = epaperData; //指令
                                 //  let order = utils.stringToBytes(orderStr);
                                 console.log("now send order to epaper, data length is : " + order.byteLength);
+                                wx.showLoading({
+                                    title: '发送中...',
+                                  })
                                 that.writeBLECharacteristicValue(order.buffer);
+                                wx.hideLoading()
+                                that.setData({progress:0});
                             }
                         })
                     });
@@ -416,9 +422,14 @@ Page({
 
     //向低功耗蓝牙设备特征值中写入二进制数据。
     //注意：必须设备的特征值支持write才可以成功调用，具体参照 characteristic 的 properties 属性
+    //30720
     writeBLECharacteristicValue: function (order) {
         var that = this;
         let byteLength = order.byteLength;
+        if(byteLength%1000 == 0)
+        {
+            that.setData({progress:Math.ceil((1- byteLength/30720)*100)});
+        }
 
         console.log("writeBLECharacteristicValue")
         wx.writeBLECharacteristicValue({
